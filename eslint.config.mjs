1f8a19ee1c9ed2c7f9bcbc6b618d3c9ac8e309e4
Huell <t0st4r@gmail.com>
2025-06-10 14:11:06 +0200

    Fix: Formatting, remove json from format check

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,37 +1,37 @@
-import js from '@eslint/js'
-import tseslint from 'typescript-eslint'
-import eslintConfigPrettier from 'eslint-config-prettier'
+import js from "@eslint/js";
+import tseslint from "typescript-eslint";
+import eslintConfigPrettier from "eslint-config-prettier";
 
 export default [
-  js.configs.recommended,
-  ...tseslint.configs.recommended,
-  eslintConfigPrettier,
-  {
-    ignores: ['node_modules/**', 'dist/**']
-  },
-  {
-    files: ['**/*.js', '**/*.jsx', '**/*.ts', '**/*.tsx'],
-    languageOptions: {
-      ecmaVersion: 'latest',
-      sourceType: 'module',
-      parser: tseslint.parser,
-      parserOptions: {
-        ecmaFeatures: {
-            jsx: true
-        }
-      }
+    js.configs.recommended,
+    ...tseslint.configs.recommended,
+    eslintConfigPrettier,
+    {
+        ignores: ["node_modules/**", "dist/**", "**/*.json"],
     },
-    rules: {
-      'no-console': 'error',
-      'no-restricted-exports': ['error', { restrictDefaultExports: { direct: true } }],
-      // used multiple times to stay consistent with sprotty
-      '@typescript-eslint/no-namespace': 'off',
-    }
-  },
-  {
-    files: ['**/*.config.ts', '**/*.config.js', '**/*.d.ts'],
-    rules: {
-      'no-restricted-exports': 'off'
-    }
-  },
-]
+    {
+        files: ["**/*.js", "**/*.jsx", "**/*.ts", "**/*.tsx"],
+        languageOptions: {
+            ecmaVersion: "latest",
+            sourceType: "module",
+            parser: tseslint.parser,
+            parserOptions: {
+                ecmaFeatures: {
+                    jsx: true,
+                },
+            },
+        },
+        rules: {
+            "no-console": "error",
+            "no-restricted-exports": ["error", { restrictDefaultExports: { direct: true } }],
+            // used multiple times to stay consistent with sprotty
+            "@typescript-eslint/no-namespace": "off",
+        },
+    },
+    {
+        files: ["**/*.config.ts", "**/*.config.js", "**/*.d.ts"],
+        rules: {
+            "no-restricted-exports": "off",
+        },
+    },
+];
