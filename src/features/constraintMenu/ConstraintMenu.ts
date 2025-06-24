064f582914a51d94afc2f031ac6d1534231c9522
Alexander Vogt <alexander_vogt@outlook.de>
2025-06-24 10:57:42 +0200

    fix line breaks

--- src/features/constraintMenu/ConstraintMenu.ts
+++ src/features/constraintMenu/ConstraintMenu.ts
@@ -133,7 +133,7 @@ export class ConstraintMenu extends AbstractUIExtension implements Switchable {
             const marker: monaco.editor.IMarkerData[] = [];
             // empty content gets accepted as valid as it represents no constraints
             if (content !== "") {
-                const lines = this.editor.getValue().split("\n");
+                const lines = this.editor.getValue().split(/\r?\n/gm);
                 const errors = lines.map(this.tree.verify, this.tree);
                 for (let i = 0; i < errors.length; i++) {
                     const lineErrors = errors[i];
